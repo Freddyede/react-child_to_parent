ff850c0abd9c71fff35b6266df35ed6f0ad15ffb
Patouillard <patouillardfranck3@gmail.com>
2021-03-10 03:08:19 +0100

    Create child components

---
+++ src/components/Toto.js
@@ -0,0 +1,12 @@
+const Toto = props => {
+    const btnReponseToto = props.leState.messageMaman !== null ? (<button onClick={props.reponse}>Réponse</button>) : (<button disabled>Réponse</button>);
+    return (
+        <div>
+            <h2>{   props.name }</h2>
+            {btnReponseToto}
+            <p>{ props.leState.messageToto }</p>
+        </div>
+    )
+}
+
+export default Toto;
